82d04d59e18648fb72b66d679e83a81b077f414b
FireInstall <kettnerl@hu-berlin.de>
2021-09-10 01:48:50 +0200

    remove unneeded variables add a system to handle the case that the bluetoot connection breaks smooth values

--- Catamaran.ino
+++ Catamaran.ino
@@ -1,21 +1,12 @@
-
-
-
-
-
-
 #include <SoftwareSerial.h>
 
 SoftwareSerial BTSerial(7, 8); // CONNECT BT RXD PIN TO ARDUINO 8 PIN | CONNECT BT TXN PIN TO ARDUINO 7 PIN
 
-int intangle = 0;
-int intstrength = 0;
-int intbutton = 0;
-float pi = 3.1416;
-bool rightbackward = 0;
-bool leftbackward = 0;
-bool lower = 0;
+bool lower = false;
 long joysticktimer;
+
+int leftspeed = 0;
+int rightspeed = 0;
 
 
 void setup()
@@ -24,7 +15,7 @@ void setup()
   pinMode(5, OUTPUT);// links direction LOW FORWARD
   pinMode(6, OUTPUT);// rechts speed
   pinMode(9, OUTPUT);// rechts direction LOW FORWARD
- 
+
   digitalWrite(3, LOW);
   digitalWrite(5, LOW);
   digitalWrite(6, LOW);
@@ -33,90 +24,90 @@ void setup()
 
   BTSerial.begin(9600);  // HC-05 default speed in AT command more
   joysticktimer = millis();
+
+  delay(200);
+
 }
 
 void loop()
-{
+{ 
   if (BTSerial.available() > 0)
   {
     joysticktimer = millis();
-    String value =
+    String val =
       BTSerial.readStringUntil('#');
-    if (value.length() == 7)
-    {
-      String angle =
-        value.substring(0, 3);
-      intangle = angle.toInt();
-      String strength =
-        value.substring(3, 6);
-      intstrength = strength.toInt();
-      String button =
-        value.substring(6, 8);
-      intbutton = button.toInt();
+    if ((val.length() == 7) && (isValidNumber(val))) {
+      int angle = val.substring(0, 3).toInt();
+      int strength = val.substring(3, 6).toInt();
+      byte button = val.substring(6, 8).toInt();
 
 
-      handlejoystick();
-      BTSerial.flush();
+      handlejoystick(angle, strength);
     }
-    if ((millis() - joysticktimer) > 300) {
+    Serial.flush();
+  }
+  if ((millis() - joysticktimer) > 300) {
       Serial.println("HERE");
-      intstrength = 0;
-      intangle = 0;
-      handlejoystick();
+      handlejoystick(0, 0);
     }
     delay(100);
-  }
 }
 
-void handlejoystick(void) {
+void handlejoystick(int angle, int strength) {
 
-  // pin 3 und 5 sind für den rechten Motor, wobei 3 die Geschwindigkeit bestimmt und 5 die Richtung
-  // pin 6 und 9 sind für den linken Motor, wobei 6 die Geschwindigkeit bestimmt und 9 die Richtung
+  // pin 3 und 5 sind für den linken Motor, wobei 3 die Geschwindigkeit bestimmt und 5 die Richtung
+  // pin 6 und 9 sind für den rechten Motor, wobei 6 die Geschwindigkeit bestimmt und 9 die Richtung
   // obere Hälfte oder untere Hälfte
-  if (intangle >= 180)lower = HIGH;
+  if (angle >= 180)lower = HIGH;
   else lower = LOW;
-  int leftspeed;
-  int rightspeed;
   float cosinus;
 
   // zunächst die rechten beiden Quadranten
-  if (intangle <= 90 || intangle >= 270) {
-    leftspeed = (int)(intstrength * 2.55);
+  if (angle <= 90 || angle >= 270) {
+    leftspeed = (int)(leftspeed * 0.9 + strength * 2.55 * 0.1);
 
-    cosinus = cos(2 * intangle * pi / 180);
+    cosinus = cos(2 * angle * PI / 180);
     // Wenn der cosinusterm größer null ist, die Richtung für rechts auf rückwärts
     // also kleiner als 45° oder größer als 315°
-    rightspeed = (int)(intstrength * 2.55) * (-cosinus);
+    rightspeed = (int)(rightspeed *0.9 + (strength * 2.55) * (-cosinus) * 0.1);
 
 
   } else {
 
-    rightspeed = (int)(intstrength * 2.55);
+    rightspeed = (int)(rightspeed *0.9 + (strength * 2.55) *0.1);
 
-    cosinus = cos(2 * intangle * pi / 180);
+    cosinus = cos(2 * angle * PI / 180);
     // Wenn der cosinusterm größer null ist, die Richtung für links auf rückwärts
 
-    leftspeed = (int)(intstrength * 2.55) * (-cosinus);
+    leftspeed = (int)(leftspeed * 0.9 + (strength * 2.0) * (-cosinus) * 0.1);
   }
   if (lower) {
     leftspeed = -leftspeed;
     rightspeed = -rightspeed;
   }
   // Schreiben der Stati und Geschwindigkeiten
-  if (leftspeed < 0) digitalWrite(9, HIGH);
-  else digitalWrite(9, LOW);
-
-  if (rightspeed < 0) digitalWrite(5, HIGH);
+  if (leftspeed < 0) digitalWrite(5, HIGH);
   else digitalWrite(5, LOW);
 
-  analogWrite(6, abs(leftspeed));
-  analogWrite(3, abs(rightspeed));
+  if (rightspeed < 0) digitalWrite(9, HIGH);
+  else digitalWrite(9, LOW);
+
+  analogWrite(3, abs(leftspeed));
+  analogWrite(6, abs(rightspeed));
 
 
+  Serial.println("Speed L: " + (String)leftspeed + "; R: " + (String)rightspeed);
+  /*Serial.print("button:  ");
+  Serial.println(intbutton);*/
 
-  
+}
 
-  Serial.print("button:  ");
-  Serial.println(intbutton);
-  
+//returns only true if every char in String is a digit
+boolean isValidNumber(String str) {
+  for (char c : str) {
+
+    if (!isDigit(c))
+      return false;
+  }
+  return true;
 }
